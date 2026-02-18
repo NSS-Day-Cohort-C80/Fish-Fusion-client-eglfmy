52682b14abd58fdd494988a6ee93aeb8f5d8e601
Eric Martinez <ericmartinez1007@gmail.com>
2026-02-18 15:54:29 -0600

    Final updated code with array methods

---
+++ fishMonger.js
@@ -0,0 +1,32 @@
+const { boatInventory } = require("./fishingBoat.js")
+
+const inventory = boatInventory();
+// fishmonger is going through the boat inventory and filtering out the fish that are too expensive or not in enough quantity. Then it is creating a new array of fish objects with the same properties as the original fish objects, but with the amount property set to 10.
+         
+// const mongerInventory = (chefPrice) => {
+//     const fishForSale = [];
+
+//     for (const fish of inventory) {
+//         if (fish.price <= chefPrice && fish.price <= 7.50 && fish.amount >= 10) {
+//             fishForSale.push({
+//                 id: fish.id,
+//                 species: fish.species,
+//                 weight: fish.weight,
+//                 price: fish.price,
+//                 amount: 5
+//             });
+//         }
+//     }
+//     return fishForSale;
+// }
+
+const mongerInventory = (chefPrice) => {
+    return inventory
+        .filter(fish => fish.price <= 7.50 &&fish.price <= chefPrice && fish.amount >= 10)
+        .map(fish => ({
+            ...fish,
+            amount: 5
+        }));
+    }
+
+module.exports = {mongerInventory};
---
+++ restaurant.js
@@ -0,0 +1,37 @@
+const { mongerInventory } = require("./fishMonger.js")
+ 
+// const fishMenu = (chefPrice) => {
+//     const fishAvailable = mongerInventory(chefPrice);
+//     let fishMenuHTML = '';
+//     for (const fish of fishAvailable) {
+//         fishMenuHTML += `
+//             <h1>Menu</h1>
+//                 <article class="menu">
+//                     <h2>${fish.species}</h2>
+//                         <section class="menu__item">${fish.species} Soup</section>
+//                         <section class="menu__item">${fish.species} Sandwich</section>
+//                         <section class="menu__item">Grilled ${fish.species}</section>
+//                 </article>`;
+//     }
+//     return fishMenuHTML;
+// };
+
+const fishMenu = (chefPrice) => {
+    const fishAvailable = mongerInventory(chefPrice);
+
+    const menuItems = fishAvailable.map(fish => `
+                <h2>${fish.species}</h2>
+                <section class="menu__item">${fish.species} Soup</section>
+                <section class="menu__item">${fish.species} Sandwich</section>
+                <section class="menu__item">Grilled ${fish.species}</section>
+            `)
+        .join('');
+            
+    return `            <h1>Menu</h1> 
+    
+            <article class="menu">${menuItems}</article>`;
+};
+
+const menu = fishMenu(7.50);
+console.log(menu);
+module.exports = {fishMenu};
